37c18b77e45c1500f63051ac9e6da8657ae905f4
JIN-DAI <daijing491@gmail.com>
2019-09-09 16:02:55 +0200

    fix bug on delta

--- rescale.m
+++ rescale.m
@@ -1,10 +1,13 @@
 function R_rescale = rescale(R_origin, delta)
-%RESCALE Summary of this function goes here
-%   Detailed explanation goes here
+%% RESCALE
+% uniformly rescale
 
 [theta,phi,vn]=anglesO(R_origin);
 
 if abs(vn-delta) > eps
+    if numel(delta) ~= numel(vn)
+        delta = repmat(delta(1), size(vn));
+    end
     R_rescale = curve3d(theta,phi,delta)';
 else
     R_rescale = R_origin;
